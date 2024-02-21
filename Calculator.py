d31c64d66dcdea6016c7a1562efa64f2ce666827
AdityaMishra2003 <155647788+AdityaMishra2003@users.noreply.github.com>
2024-02-21 19:50:52 +0530

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,17 @@
+#Caculator
+num1 = int(input("Enter the first number: "))
+operator = input("Enter the operation (+, -, *, /): ")
+num2 = float(input("Enter the second number: "))
+match operator:
+    case '+':
+        print("Sum:-",num1 + num2)
+    case '-':
+        print("Difference:-",num1 - num2)
+    case '*':
+        print("Multiplication",num1 * num2)
+    case '/':
+        print("division",num1 / num2) if num2 != 0 else "Error: Division by zero is not allowed."
+    case _:
+        print("Invalid operator. Please enter +, -, *, or /")
+
+
