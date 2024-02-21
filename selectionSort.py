2603b3bcf30cb3a58e1915e655ff51dcf8c058ac
Biswajeet-panda <121958558+Biswajeet-panda@users.noreply.github.com>
2024-02-21 18:32:18 +0530

    Add files via upload

---
+++ selectionSort.py
@@ -0,0 +1,11 @@
+def selectionsort(arr):
+    n = len(arr)
+    for i in range(n):
+        min_index = i
+        for j in range(i + 1, n):
+            if arr[j] < arr[min_index]:
+                min_index = j
+        arr[i], arr[min_index] = arr[min_index], arr[i]
+arr = [70, 39, 22, 24, 11]
+selectionsort(arr)
+print("Sorted array is:", arr)
