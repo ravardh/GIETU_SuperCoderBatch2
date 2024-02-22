d2966d91038e92d4054df5780134fdb70afb8fb2
fanibhusan2001 <subhampradhan550@gmail.com>
2024-02-22 18:25:58 +0530

    Day-3

---
+++ DSA/searching/pattern_matching.py
@@ -0,0 +1,9 @@
+if __name__ == '__main__':
+    string: str = 'ABCBBACBAABACBABC'
+    pattern: str = 'CBA'
+
+    for i in range(len(string) - len(pattern)):
+        if string[i] == 'C':
+            if string[i+1] == 'B':
+                if string[i+2] == 'A':
+                    print(i)
