6c7a01e3841c32c855eae1ba9fec1297ce5ba307
BIRENKUMARBAJUBANDHO <160488887+BIRENKUMARBAJUBANDHO@users.noreply.github.com>
2024-02-23 16:00:29 +0530

    Add files via upload

---
+++ kmpexample.py
@@ -0,0 +1,18 @@
+s = "ABCBBACBAABACBABC"
+p = "CBA"
+k = len(p)
+n = len(s)
+ans = []
+
+l,r = 0,0
+t = ""
+while(r < n):
+    t+=s[r]
+    if(r-l+1 == k):
+        if(t == p):
+            ans.append(l)
+        t = t[1:]
+        l+=1
+    r+=1
+
+print(ans)
