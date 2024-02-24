79da74687cf2acc36643f0984d40c4c9b513f82a
Kumar Kausik Sahu <132070685+kausiksahu12@users.noreply.github.com>
2024-02-24 15:28:39 +0530

    Add files via upload

---
+++ KMP.py
@@ -0,0 +1,39 @@
+#KMP Search
+def KMPSearch(pat, data):
+    M = len(pat)
+    N = len(data)
+    lps = [0] * M
+    j = 0
+    LPS(pat, M, lps)
+    i = 0
+    while i < N:
+        if pat[j] == data[i]:
+            i += 1
+            j += 1
+        if j == M:
+            print("Found pattern at index " + str(i - j))
+            j = lps[j - 1]
+
+        elif i < N and pat[j] != data[i]:
+            if j != 0:
+                j = lps[j - 1]
+            else:
+                i += 1
+def LPS(pat, M, lps):
+    length = 0
+    lps[0] = 0
+    i = 1
+    while i < M:
+        if pat[i] == pat[length]:
+            length += 1
+            lps[i] = length
+            i += 1
+        else:
+            if length != 0:
+                length = lps[length - 1]
+            else:
+                lps[i] = 0
+                i += 1
+data = "ABAABABCABABABCAABABCABAC"
+pat = "ABABC"
+KMPSearch(pat, data)
