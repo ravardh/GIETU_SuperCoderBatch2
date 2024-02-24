ad3217f79b5da0fd7fa21a920ff74959631acc63
Shubhransu Padhi <160726631+Shubhransu03@users.noreply.github.com>
2024-02-24 18:27:04 +0530

    Add files via upload

---
+++ binarysearch.py
@@ -0,0 +1,90 @@
+# Tree traversal in Python
+
+
+class Node:
+    def __init__(self, item):
+        self.left = None
+        self.right = None
+        self.val = item
+
+
+def inorder(root):
+
+    if root:
+        # Traverse left
+        inorder(root.left)
+        # Traverse root
+        print(str(root.val) + "->", end='')
+        # Traverse right
+        inorder(root.right)
+
+
+def postorder(root):
+
+    if root:
+        # Traverse left
+        postorder(root.left)
+        # Traverse right
+        postorder(root.right)
+        # Traverse root
+        print(str(root.val) + "->", end='')
+
+
+def preorder(root):
+
+    if root:
+        # Traverse root
+        print(str(root.val) + "->", end='')
+        # Traverse left
+        preorder(root.left)
+        # Traverse right
+        preorder(root.right)
+
+def LevelOrder(root):
+ 
+    # Base Case
+    if root is None:
+        return
+ 
+    # Create an empty queue
+    # for level order traversal
+    queue = []
+ 
+    # Enqueue Root and initialize height
+    queue.append(root)
+ 
+    while(len(queue) > 0):
+ 
+        # Print front of queue and
+        # remove it from queue
+        print(queue[0].val, end=" ")
+        node = queue.pop(0)
+ 
+        # Enqueue left child
+        if node.left is not None:
+            queue.append(node.left)
+ 
+        # Enqueue right child
+        if node.right is not None:
+            queue.append(node.right)
+ 
+
+root = Node(1)
+root.left = Node(2)
+root.right = Node(3)
+root.left.left = Node(4)
+root.left.right = Node(5)
+root.right.left = Node(6)
+root.right.right = Node(7)
+
+print("Inorder traversal ")
+inorder(root)
+
+print("\nPreorder traversal ")
+preorder(root)
+
+print("\nPostorder traversal ")
+postorder(root)
+
+print("\nLevel order traversal")
+LevelOrder(root)
