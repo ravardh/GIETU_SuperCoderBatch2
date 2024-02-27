0113d0d051e2442c5af71922b9f5904bf87c3577
Ashutosh Biswal <160595075+ashutoshbiswall@users.noreply.github.com>
2024-02-27 15:36:49 +0530

    Add files via upload

---
+++ maxsum.py
@@ -0,0 +1,15 @@
+def Maximum_sum(array, variable_length):
+    sum_max = float('-inf')
+    max_quadrant = None
+    for i in range(len(array) - (variable_length - 1)):
+        quadrant_sum = sum(array[i:i+variable_length])
+        if quadrant_sum > sum_max:
+            sum_max = quadrant_sum
+            max_quadrant = tuple(array[i:i+variable_length])
+    return max_quadrant, sum_max
+
+array = [1, 4, 3, 6, 7, 9, 5, 8]
+variable_length = 4
+max_quadrant, sum_max = Maximum_sum(array, variable_length)
+print("The consecutive quadrant is: ", max_quadrant)
+print("Sum of largest quadrant is:",sum_max)
