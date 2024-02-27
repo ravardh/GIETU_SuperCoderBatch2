630126a2d936de3725bd0bb3d3227e6d6ec90da3
AdityaMishra2003 <155647788+AdityaMishra2003@users.noreply.github.com>
2024-02-27 12:27:20 +0530

    Update BubbleSort.py

--- Sorting/BubbleSort.py
+++ Sorting/BubbleSort.py
@@ -5,7 +5,11 @@ for i in range (0,n):
     arr.append(int(input()))
 print("Before sorting:",arr)
 for i in range(0,n):
-    for j in range (i+1,n):
-        if(arr[i]>arr[j]):
-            arr[i],arr[j]=arr[j],arr[i]
+    flag=0
+    for j in range (0,n-i-1):
+        if(arr[j]>arr[j+1]):
+            arr[j+1],arr[j]=arr[j],arr[j+1]
+            flag=1
+    if(flag==0):
+        break
 print("After Sorting",arr)
