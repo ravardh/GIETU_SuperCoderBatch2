10f8daacd6966de2d5f32c50d462bf47bfdfe28b
Abhisek Mohanty <abhisekmohantychinua@gmail.com>
2024-02-27 09:12:48 +0530

    Day 6

--- DSA/tree/Tree.py
+++ DSA/tree/Tree.py
@@ -11,6 +11,15 @@ class Node:
         return self.right is not None
 
 
+class QObj:
+    node: Node
+    key: int
+
+    def __init__(self, node: Node, key: int):
+        self.node = node
+        self.key = key
+
+
 class Tree:
     def __init__(self):
         self.root: Node | None = None
@@ -23,6 +32,34 @@ class Tree:
 
     def postorder(self):
         self.__postorder(self.root)
+
+    def findHeight(self) -> int:
+        return self.__findHeight(self.root)
+
+    def printLeaf(self):
+        self.__printLeaf(self.root)
+
+    def topView(self):
+        queue: list = []
+        view: dict = {}
+        currKey: int = 0
+
+        queue.append(QObj(self.root, currKey))
+        while len(queue) != 0:
+            currObj: QObj = queue.pop(0)
+            currNode: Node | None = currObj.node
+            currKey = currObj.key
+
+            if currKey not in view:
+                view[currKey] = currNode
+
+            if currNode.left is not None:
+                queue.append(QObj(currNode.left, currKey - 1))
+            if currNode.right is not None:
+                queue.append(QObj(currNode.right, currKey + 1))
+
+        for key in view.keys():
+            print(view[key].data)
 
     def __preorder(self, root: Node | None):
         if root is None:
@@ -46,6 +83,23 @@ class Tree:
         self.__postorder(root.right)
         print(root.data)
 
+    def __findHeight(self, root: Node | None) -> int:
+        if root is None:
+            return 0
+        leftHeight: int = self.__findHeight(root.left)
+        rightHeight: int = self.__findHeight(root.right)
+        return max(leftHeight, rightHeight) + 1
+
+    def __printLeaf(self, root: Node | None):
+        if root is None:
+            return
+
+        if root.right is None and root.left is None:
+            print(root.data)
+            return
+        self.__printLeaf(root.left)
+        self.__printLeaf(root.right)
+
 
 if __name__ == '__main__':
     tree: Tree = Tree()
@@ -63,3 +117,9 @@ if __name__ == '__main__':
     tree.postorder()
     print("________________")
     tree.inorder()
+    print("_________________")
+    print("Height is ", tree.findHeight())
+    print("_______________")
+    tree.printLeaf()
+    print("______________")
+    tree.topView()
