4d0db38e125a8c9e3c255c9035da4fc73b5a9c89
Ashutosh Biswal <160595075+ashutoshbiswall@users.noreply.github.com>
2024-03-04 15:22:13 +0530

    Add files via upload

---
+++ bfs.py
@@ -0,0 +1,32 @@
+from collections import deque
+
+def bfs(graph, start):
+    visited = set()
+    queue = deque([start])
+    visited.add(start)
+
+    while queue:
+        vertex = queue.popleft()
+        print(vertex, end=' ')
+
+        for neighbor in graph[vertex]:
+            if neighbor not in visited:
+                queue.append(neighbor)
+                visited.add(neighbor)
+
+# Example usage:
+# Define a graph as an adjacency list
+graph = {
+    'A': ['B', 'C'],
+    'B': ['A', 'D', 'E'],
+    'C': ['A', 'F', 'G'],
+    'D': ['B'],
+    'E': ['B', 'H'],
+    'F': ['C'],
+    'G': ['C'],
+    'H': ['E']
+}
+
+# Start BFS from vertex 'A'
+print("BFS starting from vertex 'A':")
+bfs(graph, 'A')
