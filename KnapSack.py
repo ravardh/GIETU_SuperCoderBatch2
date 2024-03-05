1c8ec5dcb278a6c2a348f033cf2bf5218c9443d6
imbot <98373772+imbotcoder@users.noreply.github.com>
2024-03-05 18:20:32 +0530

    Create KnapSack.py

---
+++ KnapSack.py
@@ -0,0 +1,12 @@
+#0/1 Knapsack
+items=[0, 1, 2, 3, 4, 5, 6]
+price=[10, 5, 15, 7, 6, 18, 3]
+weight=[2, 3, 5, 7, 1, 4, 1]
+capacity=15
+rows =len(items)
+columns =capacity+1
+arr= [[0 for _ in range(columns)] for _ in range(rows)]
+for i in range(rows):
+    for w in range(columns):
+            arr[i][w]=max(arr[i-1][w],arr[i-1][w-weight[i]]+price[i])
+print("The maximum capacity is:-",arr[rows-1][columns-1])
