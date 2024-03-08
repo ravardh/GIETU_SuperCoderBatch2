3b05b0c9d26b547a411e8496490fc2f79a9328ad
rishav-kumar18 <160594267+rishav-kumar18@users.noreply.github.com>
2024-03-08 14:51:07 +0530

    Add files via upload

---
+++ scleftview.py
@@ -0,0 +1,34 @@
+class node:
+    def __init__(self,data):
+        self.left=None
+        self.data=data
+        self.right=None
+L=[0]
+def leftView(root,CL,L):
+    if root is None:
+        return
+    if(L[0]<CL):
+        print(root.data)
+        L[0]=CL
+    leftView(root.left,CL+1,L)
+    leftView(root.right,CL+1,L)
+
+root=node(1)
+root.left=node(5)
+root.right=node(7)
+root.left.left=node(2)
+root.left.right=node(4)
+root.right.right=node(17)
+root.left.left.left=node(20)
+root.left.left.left.right=node(10)
+root.left.left.left.right.left=node(21)
+root.left.right.left=node(11)
+root.left.right.right=node(3)
+root.right.right.left=node(12)
+root.right.right.right=node(16)
+root.left.right.left.left=node(14)
+root.left.right.left.right=node(19)
+root.left.right.left.right.right=node(18)
+root.left.right.left.right.right.right=node(15)
+
+leftView(root,1,L)
