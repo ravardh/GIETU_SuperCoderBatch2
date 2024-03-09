5c79689754f28b74a376c3b67b62c6dbe3e497b6
Ashutosh Pradhan <117815002+11-gx@users.noreply.github.com>
2024-03-09 15:37:25 +0530

    Add files via upload

---
+++ Dijkastra.py
@@ -0,0 +1,35 @@
+import heapq
+
+def dijkstra(graph, start):
+    distances = {node: float('inf') for node in graph}
+    distances[start] = 0
+    priority_queue = [(0, start)]
+
+    while priority_queue:
+        current_distance, current_node = heapq.heappop(priority_queue)
+
+        if current_distance > distances[current_node]:
+            continue
+
+        for neighbor, weight in graph[current_node].items():
+            distance = current_distance + weight
+            if distance < distances[neighbor]:
+                distances[neighbor] = distance
+                heapq.heappush(priority_queue, (distance, neighbor))
+
+    return distances
+
+graph = {
+    'A': {'B': 5, 'C': 3},
+    'B': {'A': 5, 'C': 2, 'D': 1},
+    'C': {'A': 3, 'B': 2, 'D': 4, 'E': 2},
+    'D': {'B': 1, 'C': 4, 'E': 3, 'F': 6},
+    'E': {'C': 2, 'D': 3, 'F': 7},
+    'F': {'D': 6, 'E': 7}
+}
+
+start_node = 'A'
+distances = dijkstra(graph, start_node)
+print("Shortest distances from node", start_node)
+for node, distance in distances.items():
+    print("To node", node + ":", distance)
