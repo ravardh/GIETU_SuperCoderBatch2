40eb683e3960b2df8a6bfea7d9198dbcda1470d3
Ashutosh Pradhan <117815002+11-gx@users.noreply.github.com>
2024-03-16 17:23:51 +0530

    Add files via upload

---
+++ inorder.py
@@ -0,0 +1,19 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+def inorder_traversal(root):
+    if root is None:
+        return []
+    return inorder_traversal(root.left) + [root.val] + inorder_traversal(root.right)
+
+root = TreeNode(1)
+root.left = TreeNode(2)
+root.right = TreeNode(3)
+root.left.left = TreeNode(4)
+root.left.right = TreeNode(5)
+
+result = inorder_traversal(root)
+print(result) 
