35d155a21eaee12681ca9875973259addae96f02
Ashutosh Pradhan <117815002+11-gx@users.noreply.github.com>
2024-03-16 17:23:18 +0530

    Add files via upload

---
+++ prims.py
@@ -0,0 +1,54 @@
+import sys
+
+class Graph:
+    def _init_(self, vertices):
+        self.V = vertices
+        self.graph = [[0 for _ in range(vertices)] for _ in range(vertices)]
+
+    def add_edge(self, u, v, weight):
+        self.graph[u][v] = weight
+        self.graph[v][u] = weight
+
+    def min_key(self, key, mst_set):
+        min_val = sys.maxsize
+        min_index = -1
+        for v in range(self.V):
+            if key[v] < min_val and not mst_set[v]:
+                min_val = key[v]
+                min_index = v
+        return min_index
+
+    def prim_mst(self):
+        parent = [-1] * self.V
+        key = [sys.maxsize] * self.V
+        key[0] = 0
+        mst_set = [False] * self.V
+
+        for _ in range(self.V):
+            u = self.min_key(key, mst_set)
+            mst_set[u] = True
+
+            for v in range(self.V):
+                if self.graph[u][v] > 0 and not mst_set[v] and key[v] > self.graph[u][v]:
+                    key[v] = self.graph[u][v]
+                    parent[v] = u
+
+        return parent
+
+    def print_mst(self, parent):
+        print("Edge \tWeight")
+        for i in range(1, self.V):
+            print(parent[i], "-", i, "\t", self.graph[i][parent[i]])
+
+
+g = Graph(5)
+g.add_edge(0, 1, 2)
+g.add_edge(0, 3, 6)
+g.add_edge(1, 2, 3)
+g.add_edge(1, 3, 8)
+g.add_edge(1, 4, 5)
+g.add_edge(2, 4, 7)
+g.add_edge(3, 4, 9)
+
+parent = g.prim_mst()
+g.print_mst(parent)
