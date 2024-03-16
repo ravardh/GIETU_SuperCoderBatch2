21a542d864662b39457560d351ca9dc5ff534e55
Ashutosh Biswal <160595075+ashutoshbiswall@users.noreply.github.com>
2024-03-16 18:24:43 +0530

    Add files via upload

---
+++ matrix.py
@@ -0,0 +1,34 @@
+def matrix_multiply(matrix1, matrix2):
+    rows1 = len(matrix1)
+    cols1 = len(matrix1[0])
+    rows2 = len(matrix2)
+    cols2 = len(matrix2[0])
+
+    if cols1 != rows2:
+        print("Cannot multiply these matrices!! Number of columns in the first matrix must be equal to the number of rows in the second matrix.")
+        return None
+
+    result = [[0 for x in range(cols2)] for x in range(rows1)]
+
+    for i in range(rows1):
+        for j in range(cols2):
+            for k in range(cols1):
+                result[i][j] += matrix1[i][k] * matrix2[k][j]
+
+    return result
+matrix1 = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+matrix2 = [
+    [9, 8, 7],
+    [6, 5, 4],
+    [3, 2, 1]
+]
+
+result = matrix_multiply(matrix1, matrix2)
+if result:
+    for row in result:
+        print(row)
