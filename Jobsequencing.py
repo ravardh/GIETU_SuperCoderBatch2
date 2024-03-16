419ec3f72162b54483e2493febe1bbbb2567b473
Subham Bidhan Sahoo <160726311+SubhamBidhan@users.noreply.github.com>
2024-03-16 18:16:09 +0530

    Jobsequencing.py

--- Jobsequencing.py
+++ Jobsequencing.py
@@ -1,21 +1,19 @@
 def job_sequencing(jobs):
     jobs.sort(key=lambda x: x[2], reverse=True)
-    max_deadline = max(jobs, key=lambda x: x[1])[1]
-    schedule = [-1] * max_deadline
+    max_deadline = max(job[1] for job in jobs)
+    slots = [False] * max_deadline
     total_profit = 0
-
+    schedule = []
     for job in jobs:
         deadline = job[1] - 1
         while deadline >= 0:
-            if schedule[deadline] == -1:
-                schedule[deadline] = job[0]
+            if not slots[deadline]:
+                slots[deadline] = True
                 total_profit += job[2]
+                schedule.append(job[0])
                 break
             deadline -= 1
-
-    return total_profit, [job_id for job_id in schedule if job_id != -1]
-
-
+    return total_profit, schedule
 jobs = [("J1", 2, 15), ("J2", 3, 1), ("J3", 1, 10), ("J4", 2, 20), ("J5", 3, 5)]
 profit, schedule = job_sequencing(jobs)
 print("Maximum profit:", profit)
