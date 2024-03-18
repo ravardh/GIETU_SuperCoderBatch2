e9472e5a9e87271ca62eb3d32776648be30f6352
Satyam Mohanty <111577463+Satyam173@users.noreply.github.com>
2024-03-18 15:20:32 +0530

    questions

--- nap_sap.py
+++ nap_sap.py
@@ -1,19 +1,19 @@
-# def knapsack(weights, values, capacity):
-#     n = len(weights)
-#     dp = [[0] * (capacity + 1) for _ in range(n + 1)]
+def knapsack(weights, values, capacity):
+    n = len(weights)
+    dp = [[0] * (capacity + 1) for _ in range(n + 1)]
 
-#     for i in range(1, n + 1):
-#         for w in range(1, capacity + 1):
+    for i in range(1, n + 1):
+        for w in range(1, capacity + 1):
             
-#             if weights[i - 1] <= w:
-#                 dp[i][w] = max(values[i - 1] + dp[i - 1][w - weights[i - 1]], dp[i - 1][w])
-#             else:
-#                 dp[i][w] = dp[i - 1][w]
+            if weights[i - 1] <= w:
+                dp[i][w] = max(values[i - 1] + dp[i - 1][w - weights[i - 1]], dp[i - 1][w])
+            else:
+                dp[i][w] = dp[i - 1][w]
 
-#     return dp[n][capacity]
+    return dp[n][capacity]
 
-# res=knapsack([2,3,5,7,1,4,1],[10,5,15,7,6,18,3],15)
-# print(res)
+res=knapsack([2,3,5,7,1,4,1],[10,5,15,7,6,18,3],15)
+print(res)
 
 def knapsack(weights, values, capacity):
     n = len(weights)
