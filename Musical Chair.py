359b5af11fb05e7817424a4e5346220be0293e8f
P Satya Sandeep <94365431+paidisettysatyasandeeep@users.noreply.github.com>
2024-03-19 11:25:57 +0530

    Add files via upload

---
+++ Musical Chair.py
@@ -0,0 +1,16 @@
+def find_winner(N, K):
+    chairs = list(range(1, N + 1))  
+    
+    index = 0 
+    
+    while len(chairs) > 1:
+        index = (index + K - 1) % len(chairs)
+        print(chairs[index])
+        chairs.pop(index)
+        
+    
+    return chairs[0]
+
+N = 14
+K = 20
+print("Winner:", find_winner(N, K))
