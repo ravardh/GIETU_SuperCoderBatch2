0ae68e9f970ba0d3f0075a1c97b8ee6a23c6750c
P Satya Sandeep <94365431+paidisettysatyasandeeep@users.noreply.github.com>
2024-03-20 18:11:57 +0530

    Kth smallest element.py

--- Kth smallest element.py
+++ Kth smallest element.py
@@ -1,57 +1,41 @@
 class Node:
-
-	def __init__(self, key):
-
-		self.data = key
-		self.left = None
-		self.right = None
+    def __init__(self, key):
+        self.data = key
+        self.left = None
+        self.right = None
 
 def insert(root, x):
-
-	if (root == None):
-		return Node(x)
-	if (x < root.data):
-		root.left = insert(root.left, x)
-	elif (x > root.data):
-		root.right = insert(root.right, x)
-	return root
+    if (root == None):
+        return Node(x)
+    if (x < root.data):
+        root.left = insert(root.left, x)
+    elif (x > root.data):
+        root.right = insert(root.right, x)
+    return root
 
 def kthSmallest(root):
-
-	global k
-
-	if (root == None):
-		return None
-
-	left = kthSmallest(root.left)
-
-	if (left != None):
-		return left
-
-	k -= 1
-	if (k == 0):
-		return root
-
-	return kthSmallest(root.right)
+    global k
+    if (root == None):
+        return None
+    left = kthSmallest(root.left)
+    if (left != None):
+        return left
+    k -= 1
+    if (k == 0):
+        return root
+    return kthSmallest(root.right)
 
 def printKthSmallest(root):
-
-	res = kthSmallest(root)
-
-	if (res == None):
-		print("There are less than k nodes in the BST")
-	else:
-		print("K-th Smallest Element is ", res.data)
-
+    res = kthSmallest(root)
+    if (res == None):
+        print("There are less than k nodes in the BST")
+    else:
+        print("K-th Smallest Element is ", res.data)
 
 if __name__ == '__main__':
-
-	root = None
-	keys = [20, 8, 22, 4, 12, 10, 14]
-
-	for x in keys:
-		root = insert(root, x)
-
-	k = 5
-
-	printKthSmallest(root)
+    root = None
+    keys = [20, 8, 22, 4, 12, 10, 14]
+    for x in keys:
+        root = insert(root, x)
+    k = 5
+    printKthSmallest(root)
